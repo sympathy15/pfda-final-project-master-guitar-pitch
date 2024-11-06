cd0a8cca79c065b00190a1996c919ddce288fa8c
Leo Pozos <leo.pozos05@gmail.com>
2024-11-05 21:14:19 -0600

    testing buttons and sound

--- master_guitar_pitch.py
+++ master_guitar_pitch.py
@@ -1,7 +1,37 @@
 import pygame
 
 def main():
-    print("test")
+    pygame.init()
+    pygame.mixer.init()
+    pygame.display.set_caption("Master Guitar Pitch")
+    screen = pygame.display.set_mode((800, 600))
+    sound = pygame.mixer.Sound('test.wav')
+    
+    
+    button_x, button_y = 400, 300
+    button_wh = 200
+
+    font = pygame.font.Font(None, 36)
+
+    # sound.play()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        screen.fill(pygame.Color(0,0,0))
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        txt = font.render("Text", True, (255,255,255))
+        screen.blit(txt, (50, 50))
+        if button_x < mouse_x < button_x + button_wh and button_y < mouse_y < button_y + button_wh:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, (255, 0, 0), (button_x, button_y, button_wh, button_wh))
+        else:
+            pygame.draw.rect(screen, (255, 255, 0), (button_x, button_y, button_wh, button_wh))
+        
+        pygame.display.flip()
+    pygame.quit
 
 if __name__ == "__main__":
     main()
