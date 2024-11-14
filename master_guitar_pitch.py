4aef2f2a3a2e5c9f661adc54eb8f35a3fbf664ed
Leo Pozos <leo.pozos05@gmail.com>
2024-11-14 13:07:16 -0600

    added lives, guitar image, and optimizations

--- master_guitar_pitch.py
+++ master_guitar_pitch.py
@@ -27,21 +27,23 @@ def main():
 
     padding = 27
     button_wh = 100
-    a_button_x, a_button_y = padding, 300
-    ab_button_x, ab_button_y = a_button_x + button_wh + padding, 300
-    b_button_x, b_button_y = ab_button_x + button_wh + padding, 300
-    c_button_x, c_button_y = b_button_x + button_wh + padding, 300
-    cd_button_x, cd_button_y = c_button_x + button_wh + padding, 300
-    d_button_x, d_button_y = cd_button_x + button_wh + padding, 300
+    height_a = 350
+    height_b = 480
+    a_button_x, a_button_y = padding, height_a
+    ab_button_x, ab_button_y = a_button_x + button_wh + padding, height_a
+    b_button_x, b_button_y = ab_button_x + button_wh + padding, height_a
+    c_button_x, c_button_y = b_button_x + button_wh + padding, height_a
+    cd_button_x, cd_button_y = c_button_x + button_wh + padding, height_a
+    d_button_x, d_button_y = cd_button_x + button_wh + padding, height_a
 
-    de_button_x, de_button_y = padding, + 430
-    e_button_x, e_button_y = de_button_x + button_wh + padding, 430
-    f_button_x, f_button_y = e_button_x + button_wh + padding, 430
-    fg_button_x, fg_button_y = f_button_x + button_wh + padding, 430
-    g_button_x, g_button_y = fg_button_x + button_wh + padding, 430
-    ga_button_x, ga_button_y = g_button_x + button_wh + padding, 430
+    de_button_x, de_button_y = padding, + height_b
+    e_button_x, e_button_y = de_button_x + button_wh + padding, height_b
+    f_button_x, f_button_y = e_button_x + button_wh + padding, height_b
+    fg_button_x, fg_button_y = f_button_x + button_wh + padding, height_b
+    g_button_x, g_button_y = fg_button_x + button_wh + padding, height_b
+    ga_button_x, ga_button_y = g_button_x + button_wh + padding, height_b
     
-    answer = random.randrange(0,13)
+    answer = random.randrange(0,12)
     shuffle = True
     sound_played = False
 
@@ -58,58 +60,55 @@ def main():
     ED = pygame.mixer.Sound('guitar-notes/ED.mp3')
     EEb = pygame.mixer.Sound('guitar-notes/EEb.mp3')
 
+    sounds = [EA, EBb, EB, EC, EDb, ED, EEb, EE, EF, EGb, EG, EAb]
+    guitar = pygame.image.load("guitar_PNG3361.png").convert_alpha()
+    lives = 3
+
     running = True
     while running:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+        
         screen.fill(pygame.Color(0,0,0))
         mouse_x, mouse_y = pygame.mouse.get_pos()
+        screen.blit(pygame.transform.scale(guitar, (2048//1.5, 993//1.5)),(0,-100))
 
         if shuffle == True:
-            answer = random.randrange(0,13)
+            answer = random.randrange(0,12)
             sound_played = False
             shuffle = False
-
     
         if sound_played == False:
-            if answer == 0:
-                EA.play()
-            elif answer == 1:
-                EBb.play()
-            elif answer == 2:
-                EB.play()
-            elif answer == 3:
-                EC.play()
-            elif answer == 4:
-                EDb.play()
-            elif answer == 5:
-                ED.play()
-            elif answer == 6:
-                EEb.play()
-            elif answer == 7:
-                EE.play()
-            elif answer == 8:
-                EF.play()
-            elif answer == 9:
-                EGb.play()
-            elif answer == 10:
-                EG.play()
-            elif answer == 11:
-                EAb.play()
-
-        sound_played = True
+            sounds[answer].play()
+            sound_played = True
+        
         font = pygame.font.Font(None, 50)
         text = f"attempts: {attempts}/5"
         txt = font.render(text, True, (255,255,255))
         screen.blit(txt, (10, 10))
         
+        make_lives(screen, attempts)
+        
+        make_button(screen, a_color, a_button_x, a_button_y, button_wh, "A")
+        make_button(screen, ab_color, ab_button_x, ab_button_y, button_wh, "A#/Bb")
+        make_button(screen, b_color, b_button_x, b_button_y, button_wh, "B")
+        make_button(screen, c_color, c_button_x, c_button_y, button_wh, "C")
+        make_button(screen, cd_color, cd_button_x, cd_button_y, button_wh, "C#/Db")
+        make_button(screen, d_color, d_button_x, d_button_y, button_wh, "D")
+        make_button(screen, de_color, de_button_x, de_button_y, button_wh, "D#/Eb")
+        make_button(screen, e_color, e_button_x, e_button_y, button_wh, "E")
+        make_button(screen, f_color, f_button_x, f_button_y, button_wh, "F")
+        make_button(screen, fg_color, fg_button_x, fg_button_y, button_wh, "F#/Gb")
+        make_button(screen, g_color, g_button_x, g_button_y, button_wh, "G")
+        make_button(screen, ga_color, ga_button_x, ga_button_y, button_wh, "G#/Ab")
+
         shuffle = shuffle or check_mouse(a_button_x, a_button_y, mouse_x, mouse_y, button_wh, event, screen, a_color, white, 0, answer)
         shuffle = shuffle or check_mouse(ab_button_x, ab_button_y, mouse_x, mouse_y, button_wh, event, screen, ab_color, white, 1, answer)
         shuffle = shuffle or check_mouse(b_button_x, b_button_y, mouse_x, mouse_y, button_wh, event, screen, b_color, white, 2, answer)
         shuffle = shuffle or check_mouse(c_button_x, c_button_y, mouse_x, mouse_y, button_wh, event, screen, c_color, white, 3, answer)
-        shuffle = shuffle or check_mouse(cd_button_x, cd_button_y, mouse_x, mouse_y, button_wh, event, screen, cd_color, white, 4, answer)
         shuffle = shuffle or check_mouse(d_button_x, d_button_y, mouse_x, mouse_y, button_wh, event, screen, d_color, white, 5, answer)
+        shuffle = shuffle or check_mouse(cd_button_x, cd_button_y, mouse_x, mouse_y, button_wh, event, screen, cd_color, white, 4, answer)
         shuffle = shuffle or check_mouse(de_button_x, de_button_y, mouse_x, mouse_y, button_wh, event, screen, de_color, white, 6, answer)
         shuffle = shuffle or check_mouse(e_button_x, e_button_y, mouse_x, mouse_y, button_wh, event, screen, e_color, white, 7, answer)
         shuffle = shuffle or check_mouse(f_button_x, f_button_y, mouse_x, mouse_y, button_wh, event, screen, f_color, white, 8, answer)
@@ -125,15 +124,15 @@ def check_mouse(button_x, button_y, mouse_x, mouse_y, button_wh, event, screen, 
     shuffle = False
     global attempts
 
-
     if button_x < mouse_x < button_x + button_wh and button_y < mouse_y < button_y + button_wh:
-        
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             make_button(screen, color, button_x, button_y, button_wh, text[num])
             if answer == num:
                 print("correct")
                 make_checkmark(screen)
+                pygame.display.flip()
+                pygame.time.delay(1000)
                 time.sleep(1)
                 shuffle = True
                 attempts = 5
@@ -141,6 +140,8 @@ def check_mouse(button_x, button_y, mouse_x, mouse_y, button_wh, event, screen, 
                 print("incorrect")
                 attempts -= 1
                 make_x(screen)
+                pygame.display.flip()
+                pygame.time.delay(1000)
                 time.sleep(1)
                 if attempts == 0:
                     shuffle = True 
@@ -152,15 +153,120 @@ def check_mouse(button_x, button_y, mouse_x, mouse_y, button_wh, event, screen, 
     
     return shuffle
 
+def make_lives(screen, attempts):
+    
+    if attempts == 5:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((255,49,49))
+        heart2.fill((255,49,49))
+        heart3.fill((255,49,49))
+        heart4.fill((255,49,49))
+        heart5.fill((255,49,49))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+    if attempts == 4:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((255,49,49))
+        heart2.fill((255,49,49))
+        heart3.fill((255,49,49))
+        heart4.fill((255,49,49))
+        heart5.fill((0,0,0))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+    if attempts == 3:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((255,49,49))
+        heart2.fill((255,49,49))
+        heart3.fill((255,49,49))
+        heart4.fill((0,0,0))
+        heart5.fill((0,0,0))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+    if attempts == 2:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((255,49,49))
+        heart2.fill((255,49,49))
+        heart3.fill((0,0,0))
+        heart4.fill((0,0,0))
+        heart5.fill((0,0,0))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+    if attempts == 1:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((255,49,49))
+        heart2.fill((0,0,0))
+        heart3.fill((0,0,0))
+        heart4.fill((0,0,0))
+        heart5.fill((0,0,0))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+    if attempts == 0:
+        heart1 = pygame.Surface((50,50))
+        heart2 = pygame.Surface((50, 50))
+        heart3 = pygame.Surface((50, 50))
+        heart4 = pygame.Surface((50, 50))
+        heart5 = pygame.Surface((50, 50))
+        heart1.fill((0,0,0))
+        heart2.fill((0,0,0))
+        heart3.fill((0,0,0))
+        heart4.fill((0,0,0))
+        heart5.fill((0,0,0))
+        screen.blit(heart1, (430,10))
+        screen.blit(heart2, (490,10))
+        screen.blit(heart3, (550,10))
+        screen.blit(heart4, (610,10))
+        screen.blit(heart5, (670,10))
+
+    screen.blit(heart1, (430,10))
+    screen.blit(heart2, (490,10))
+    screen.blit(heart3, (550,10))
+    screen.blit(heart4, (610,10))
+    screen.blit(heart5, (670,10))
+
 def make_checkmark(screen):
     check = pygame.Surface((50,50))
     check.fill((0,255,0))
-    screen.blit(check, (400,200))
+    screen.blit(check, (368,200))
 
 def make_x(screen):
     x = pygame.Surface((50,50))
     x.fill((255,49,49))
-    screen.blit(x, (400,200))
+    screen.blit(x, (368,200))
 
 def make_button(screen, color, button_x, button_y, button_wh, text):
     large_txt = ["A", "B", "C", "D", "E", "F", "G"]
